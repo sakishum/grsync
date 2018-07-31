299c8fed668c9a7c1909e9b1e7618c4eefb1d929
Denis Hananein <i@zloylos.me>
2018-07-31 23:50:01 +0300

    Add rsync options with tests

--- rsync/rsync.go
+++ rsync/rsync.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 // Rsync is wrapper under rsync
@@ -65,9 +67,112 @@ type Options struct {
 	// XAttrs preserve extended attributes
 	XAttrs bool
 	// Owner preserve owner (super-user only)
-	Owner         bool
-	Compress      bool
+	Owner bool
+	// Group preserve group
+	Group bool
+	// Devices preserve device files (super-user only)
+	Devices bool
+	// Specials preserve special files
+	Specials bool
+	// Times preserve modification times
+	Times bool
+	// omit directories from --times
+	OmitDirTimes bool
+	// Super receiver attempts super-user activities
+	Super bool
+	// FakeSuper store/recover privileged attrs using xattrs
+	FakeSuper bool
+	// Sparce handle sparse files efficiently
+	Sparse bool
+	// DryRun perform a trial run with no changes made
+	DryRun bool
+	// WholeFile copy files whole (w/o delta-xfer algorithm)
+	WhileFile bool
+	// OneFileSystem don't cross filesystem boundaries
+	OneFileSystem bool
+	// BlockSize block-size=SIZE force a fixed checksum block-size
+	BlockSize int
+	// Rsh -rsh=COMMAND specify the remote shell to use
+	Rsh string
+	// RsyncProgramm rsync-path=PROGRAM specify the rsync to run on remote machine
+	RsyncProgramm string
+	// Existing skip creating new files on receiver
+	Existing bool
+	// IgnoreExisting skip updating files that exist on receiver
+	IgnoreExisting bool
+	// RemoveSourceFiles sender removes synchronized files (non-dir)
+	RemoveSourceFiles bool
+	// Delete delete extraneous files from dest dirs
+	Delete bool
+	// DeleteBefore receiver deletes before transfer, not during
+	DeleteBefore bool
+	// DeleteDuring receiver deletes during the transfer
+	DeleteDuring bool
+	// DeleteDelay find deletions during, delete after
+	DeleteDelay bool
+	// DeleteAfter receiver deletes after transfer, not during
+	DeleteAfter bool
+	// DeleteExcluded also delete excluded files from dest dirs
+	DeleteExcluded bool
+	// IgnoreErrors delete even if there are I/O errors
+	IgnoreErrors bool
+	// Force deletion of dirs even if not empty
+	Force bool
+	// MaxDelete max-delete=NUM don't delete more than NUM files
+	MaxDelete int
+	// MaxSize max-size=SIZE don't transfer any file larger than SIZE
+	MaxSize int
+	// MinSize don't transfer any file smaller than SIZE
+	MinSize int
+	// Partial keep partially transferred files
+	Partial bool
+	// PartialDir partial-dir=DIR
+	PartialDir string
+	// DelayUpdates put all updated files into place at end
+	DelayUpdates bool
+	// PruneEmptyDirs prune empty directory chains from file-list
+	PruneEmptyDirs bool
+	// NumericIDs don't map uid/gid values by user/group name
+	NumericIDs bool
+	// Timeout timeout=SECONDS set I/O timeout in seconds
+	Timeout int
+	// Contimeout contimeout=SECONDS set daemon connection timeout in seconds
+	Contimeout int
+	// IgnoreTimes don't skip files that match size and time
+	IgnoreTimes bool
+	// SizeOnly skip files that match in size
+	SizeOnly bool
+	// ModifyWindow modify-window=NUM compare mod-times with reduced accuracy
+	ModifyWindow bool
+	// TempDir temp-dir=DIR create temporary files in directory DIR
+	TempDir string
+	// Fuzzy find similar file for basis if no dest file
+	Fuzzy bool
+	// CompareDest compare-dest=DIR also compare received files relative to DIR
+	CompareDest string
+	// CopyDest copy-dest=DIR ... and include copies of unchanged files
+	CopyDest string
+	// LinkDest link-dest=DIR hardlink to files in DIR when unchanged
+	LinkDest string
+	// Compress file data during the transfer
+	Compress bool
+	// CompressLevel explicitly set compression level
 	CompressLevel int
+	// SkipCompress skip-compress=LIST skip compressing files with suffix in LIST
+	SkipCompress []string
+	// CVSExclude auto-ignore files in the same way CVS does
+	CVSExclude bool
+	// Stats give some file-transfer stats
+	Stats bool
+	// HumanReadable output numbers in a human-readable format
+	HumanReadable bool
+	// Progress show progress during transfer
+	Progress bool
+
+	// ipv4
+	IPv4 bool
+	// ipv6
+	IPv6 bool
 }
 
 // StdoutPipe returns a pipe that will be connected to the command's
@@ -137,6 +242,272 @@ func getArguments(options Options) []string {
 
 	if options.Append {
 		arguments = append(arguments, "--append")
+	}
+
+	if options.AppendVerify {
+		arguments = append(arguments, "--append-verify")
+	}
+
+	if options.Dirs {
+		arguments = append(arguments, "--dirs")
+	}
+
+	if options.Links {
+		arguments = append(arguments, "--links")
+	}
+
+	if options.CopyLinks {
+		arguments = append(arguments, "--copy-links")
+	}
+
+	if options.CopyUnsafeLinks {
+		arguments = append(arguments, "--copy-unsafe-links")
+	}
+
+	if options.SafeLinks {
+		arguments = append(arguments, "--safe-links")
+	}
+
+	if options.CopyDirLinks {
+		arguments = append(arguments, "--copy-dir-links")
+	}
+
+	if options.KeepDirLinks {
+		arguments = append(arguments, "--keep-dir-links")
+	}
+
+	if options.HardLinks {
+		arguments = append(arguments, "--hard-links")
+	}
+
+	if options.Perms {
+		arguments = append(arguments, "--perms")
+	}
+
+	if options.Executability {
+		arguments = append(arguments, "--executability")
+	}
+
+	if options.ACLs {
+		arguments = append(arguments, "--acls")
+	}
+
+	if options.XAttrs {
+		arguments = append(arguments, "--xattrs")
+	}
+
+	/////////
+
+	if options.Owner {
+		arguments = append(arguments, "--owner")
+	}
+
+	if options.Group {
+		arguments = append(arguments, "--group")
+	}
+
+	if options.Devices {
+		arguments = append(arguments, "--devices")
+	}
+
+	if options.Specials {
+		arguments = append(arguments, "--specials")
+	}
+
+	if options.Times {
+		arguments = append(arguments, "--times")
+	}
+
+	if options.OmitDirTimes {
+		arguments = append(arguments, "--omit-dir-times")
+	}
+
+	if options.Super {
+		arguments = append(arguments, "--super")
+	}
+
+	if options.FakeSuper {
+		arguments = append(arguments, "--fake-super")
+	}
+
+	if options.Sparse {
+		arguments = append(arguments, "--sparse")
+	}
+
+	if options.DryRun {
+		arguments = append(arguments, "--dry-run")
+	}
+
+	if options.WhileFile {
+		arguments = append(arguments, "--while-file")
+	}
+
+	if options.OneFileSystem {
+		arguments = append(arguments, "--one-file-system")
+	}
+
+	if options.BlockSize > 0 {
+		arguments = append(arguments, "--block-size="+strconv.Itoa(options.BlockSize))
+	}
+
+	if options.Rsh != "" {
+		arguments = append(arguments, "--rsh="+options.Rsh)
+	}
+
+	if options.RsyncProgramm != "" {
+		arguments = append(arguments, "--rsync-programm="+options.RsyncProgramm)
+	}
+
+	if options.Existing {
+		arguments = append(arguments, "--existing")
+	}
+
+	if options.IgnoreExisting {
+		arguments = append(arguments, "--ignore-existing")
+	}
+
+	if options.RemoveSourceFiles {
+		arguments = append(arguments, "--remove-source-files")
+	}
+
+	if options.Delete {
+		arguments = append(arguments, "--delete")
+	}
+
+	if options.DeleteBefore {
+		arguments = append(arguments, "--delete-before")
+	}
+
+	if options.DeleteDuring {
+		arguments = append(arguments, "--delete-during")
+	}
+
+	if options.DeleteDelay {
+		arguments = append(arguments, "--delete-delay")
+	}
+
+	if options.DeleteAfter {
+		arguments = append(arguments, "--delete-after")
+	}
+
+	if options.DeleteExcluded {
+		arguments = append(arguments, "--delete-excluded")
+	}
+
+	if options.IgnoreErrors {
+		arguments = append(arguments, "--ignore-errors")
+	}
+
+	if options.Force {
+		arguments = append(arguments, "--force")
+	}
+
+	if options.MaxDelete > 0 {
+		arguments = append(arguments, "--max-delete="+strconv.Itoa(options.MaxDelete))
+	}
+
+	if options.MaxSize > 0 {
+		arguments = append(arguments, "--max-size="+strconv.Itoa(options.MaxSize))
+	}
+
+	if options.MinSize > 0 {
+		arguments = append(arguments, "--min-size="+strconv.Itoa(options.MinSize))
+	}
+
+	if options.Partial {
+		arguments = append(arguments, "--partial")
+	}
+
+	if options.PartialDir != "" {
+		arguments = append(arguments, "--partial-dir="+options.PartialDir)
+	}
+
+	if options.DelayUpdates {
+		arguments = append(arguments, "--delay-updates")
+	}
+
+	if options.PruneEmptyDirs {
+		arguments = append(arguments, "--prune-empty-dirs")
+	}
+
+	if options.NumericIDs {
+		arguments = append(arguments, "--numeric-ids")
+	}
+
+	if options.Timeout > 0 {
+		arguments = append(arguments, "--timeout="+strconv.Itoa(options.Timeout))
+	}
+
+	if options.Contimeout > 0 {
+		arguments = append(arguments, "--contimeout="+strconv.Itoa(options.Contimeout))
+	}
+
+	if options.IgnoreTimes {
+		arguments = append(arguments, "--ignore-times")
+	}
+
+	if options.SizeOnly {
+		arguments = append(arguments, "--size-only")
+	}
+
+	if options.ModifyWindow {
+		arguments = append(arguments, "--modify-window")
+	}
+
+	if options.TempDir != "" {
+		arguments = append(arguments, "--temp-dir="+options.TempDir)
+	}
+
+	if options.Fuzzy {
+		arguments = append(arguments, "--fuzzy")
+	}
+
+	if options.CompareDest != "" {
+		arguments = append(arguments, "--compare-dest="+options.CompareDest)
+	}
+
+	if options.CopyDest != "" {
+		arguments = append(arguments, "--copy-dest="+options.CopyDest)
+	}
+
+	if options.LinkDest != "" {
+		arguments = append(arguments, "--link-dest="+options.LinkDest)
+	}
+
+	if options.Compress {
+		arguments = append(arguments, "--compress")
+	}
+
+	if options.CompressLevel > 0 {
+		arguments = append(arguments, "--compress-level="+strconv.Itoa(options.CompressLevel))
+	}
+
+	if len(options.SkipCompress) > 0 {
+		arguments = append(arguments, "--skip-compress="+strings.Join(options.SkipCompress, ","))
+	}
+
+	if options.CVSExclude {
+		arguments = append(arguments, "--cvs-exclude")
+	}
+
+	if options.Stats {
+		arguments = append(arguments, "--stats")
+	}
+
+	if options.HumanReadable {
+		arguments = append(arguments, "--human-readable")
+	}
+
+	if options.Progress {
+		arguments = append(arguments, "--progress")
+	}
+
+	if options.IPv4 {
+		arguments = append(arguments, "--ipv4")
+	}
+
+	if options.IPv6 {
+		arguments = append(arguments, "--ipv6")
 	}
 
 	return arguments
--- rsync/rsync_test.go
+++ rsync/rsync_test.go
@@ -76,4 +76,466 @@ func TestParseArguments(t *testing.T) {
 		})
 		assert.Contains(t, args, "--append")
 	})
+
+	t.Run("--append-verify", func(t *testing.T) {
+		args := getArguments(Options{
+			AppendVerify: true,
+		})
+		assert.Contains(t, args, "--append-verify")
+	})
+
+	t.Run("--dirs", func(t *testing.T) {
+		args := getArguments(Options{
+			Dirs: true,
+		})
+		assert.Contains(t, args, "--dirs")
+	})
+
+	t.Run("--links", func(t *testing.T) {
+		args := getArguments(Options{
+			Links: true,
+		})
+		assert.Contains(t, args, "--links")
+	})
+
+	t.Run("--copy-links", func(t *testing.T) {
+		args := getArguments(Options{
+			CopyLinks: true,
+		})
+		assert.Contains(t, args, "--copy-links")
+	})
+
+	t.Run("--copy-unsafe-links", func(t *testing.T) {
+		args := getArguments(Options{
+			CopyUnsafeLinks: true,
+		})
+		assert.Contains(t, args, "--copy-unsafe-links")
+	})
+
+	t.Run("--safe-links", func(t *testing.T) {
+		args := getArguments(Options{
+			SafeLinks: true,
+		})
+		assert.Contains(t, args, "--safe-links")
+	})
+
+	t.Run("--copy-dir-links", func(t *testing.T) {
+		args := getArguments(Options{
+			CopyDirLinks: true,
+		})
+		assert.Contains(t, args, "--copy-dir-links")
+	})
+
+	t.Run("--keep-dir-links", func(t *testing.T) {
+		args := getArguments(Options{
+			KeepDirLinks: true,
+		})
+		assert.Contains(t, args, "--keep-dir-links")
+	})
+
+	t.Run("--hard-links", func(t *testing.T) {
+		args := getArguments(Options{
+			HardLinks: true,
+		})
+		assert.Contains(t, args, "--hard-links")
+	})
+
+	t.Run("--perms", func(t *testing.T) {
+		args := getArguments(Options{
+			Perms: true,
+		})
+		assert.Contains(t, args, "--perms")
+	})
+
+	t.Run("--executability", func(t *testing.T) {
+		args := getArguments(Options{
+			Executability: true,
+		})
+		assert.Contains(t, args, "--executability")
+	})
+
+	t.Run("--acls", func(t *testing.T) {
+		args := getArguments(Options{
+			ACLs: true,
+		})
+		assert.Contains(t, args, "--acls")
+	})
+
+	t.Run("--xattrs", func(t *testing.T) {
+		args := getArguments(Options{
+			XAttrs: true,
+		})
+		assert.Contains(t, args, "--xattrs")
+	})
+
+	t.Run("--owner", func(t *testing.T) {
+		args := getArguments(Options{
+			Owner: true,
+		})
+		assert.Contains(t, args, "--owner")
+	})
+
+	t.Run("--group", func(t *testing.T) {
+		args := getArguments(Options{
+			Group: true,
+		})
+		assert.Contains(t, args, "--group")
+	})
+
+	t.Run("--devices", func(t *testing.T) {
+		args := getArguments(Options{
+			Devices: true,
+		})
+		assert.Contains(t, args, "--devices")
+	})
+
+	t.Run("--specials", func(t *testing.T) {
+		args := getArguments(Options{
+			Specials: true,
+		})
+		assert.Contains(t, args, "--specials")
+	})
+
+	t.Run("--times", func(t *testing.T) {
+		args := getArguments(Options{
+			Times: true,
+		})
+		assert.Contains(t, args, "--times")
+	})
+
+	t.Run("--omit-dir-times", func(t *testing.T) {
+		args := getArguments(Options{
+			OmitDirTimes: true,
+		})
+		assert.Contains(t, args, "--omit-dir-times")
+	})
+
+	t.Run("--super", func(t *testing.T) {
+		args := getArguments(Options{
+			Super: true,
+		})
+		assert.Contains(t, args, "--super")
+	})
+
+	t.Run("--fake-super", func(t *testing.T) {
+		args := getArguments(Options{
+			FakeSuper: true,
+		})
+		assert.Contains(t, args, "--fake-super")
+	})
+
+	t.Run("--sparse", func(t *testing.T) {
+		args := getArguments(Options{
+			Sparse: true,
+		})
+		assert.Contains(t, args, "--sparse")
+	})
+
+	t.Run("--dry-run", func(t *testing.T) {
+		args := getArguments(Options{
+			DryRun: true,
+		})
+		assert.Contains(t, args, "--dry-run")
+	})
+
+	t.Run("--while-file", func(t *testing.T) {
+		args := getArguments(Options{
+			WhileFile: true,
+		})
+		assert.Contains(t, args, "--while-file")
+	})
+
+	t.Run("--one-file-system", func(t *testing.T) {
+		args := getArguments(Options{
+			OneFileSystem: true,
+		})
+		assert.Contains(t, args, "--one-file-system")
+	})
+
+	t.Run("--block-size", func(t *testing.T) {
+		args := getArguments(Options{
+			BlockSize: 1,
+		})
+		assert.Contains(t, args, "--block-size=1")
+	})
+
+	t.Run("--rsh", func(t *testing.T) {
+		args := getArguments(Options{
+			Rsh: "test",
+		})
+		assert.Contains(t, args, "--rsh=test")
+	})
+
+	t.Run("--rsync-programm", func(t *testing.T) {
+		args := getArguments(Options{
+			RsyncProgramm: "test",
+		})
+		assert.Contains(t, args, "--rsync-programm=test")
+	})
+
+	t.Run("--existing", func(t *testing.T) {
+		args := getArguments(Options{
+			Existing: true,
+		})
+		assert.Contains(t, args, "--existing")
+	})
+
+	t.Run("--ignore-existing", func(t *testing.T) {
+		args := getArguments(Options{
+			IgnoreExisting: true,
+		})
+		assert.Contains(t, args, "--ignore-existing")
+	})
+
+	t.Run("--remove-source-files", func(t *testing.T) {
+		args := getArguments(Options{
+			RemoveSourceFiles: true,
+		})
+		assert.Contains(t, args, "--remove-source-files")
+	})
+
+	t.Run("", func(t *testing.T) {
+		args := getArguments(Options{
+			Delete: true,
+		})
+		assert.Contains(t, args, "--delete")
+	})
+
+	t.Run("--delete", func(t *testing.T) {
+		args := getArguments(Options{
+			DeleteBefore: true,
+		})
+		assert.Contains(t, args, "--delete-before")
+	})
+
+	t.Run("--delete-during", func(t *testing.T) {
+		args := getArguments(Options{
+			DeleteDuring: true,
+		})
+		assert.Contains(t, args, "--delete-during")
+	})
+
+	t.Run("--delete-delay", func(t *testing.T) {
+		args := getArguments(Options{
+			DeleteDelay: true,
+		})
+		assert.Contains(t, args, "--delete-delay")
+	})
+
+	t.Run("--delete-after", func(t *testing.T) {
+		args := getArguments(Options{
+			DeleteAfter: true,
+		})
+		assert.Contains(t, args, "--delete-after")
+	})
+
+	t.Run("--delete-excluded", func(t *testing.T) {
+		args := getArguments(Options{
+			DeleteExcluded: true,
+		})
+		assert.Contains(t, args, "--delete-excluded")
+	})
+
+	t.Run("--ignore-errors", func(t *testing.T) {
+		args := getArguments(Options{
+			IgnoreErrors: true,
+		})
+		assert.Contains(t, args, "--ignore-errors")
+	})
+
+	t.Run("--force", func(t *testing.T) {
+		args := getArguments(Options{
+			Force: true,
+		})
+		assert.Contains(t, args, "--force")
+	})
+
+	t.Run("--max-delete", func(t *testing.T) {
+		args := getArguments(Options{
+			MaxDelete: 1,
+		})
+		assert.Contains(t, args, "--max-delete=1")
+	})
+
+	t.Run("--max-size", func(t *testing.T) {
+		args := getArguments(Options{
+			MaxSize: 1,
+		})
+		assert.Contains(t, args, "--max-size=1")
+	})
+
+	t.Run("--min-size", func(t *testing.T) {
+		args := getArguments(Options{
+			MinSize: 1,
+		})
+		assert.Contains(t, args, "--min-size=1")
+	})
+
+	t.Run("--partial", func(t *testing.T) {
+		args := getArguments(Options{
+			Partial: true,
+		})
+		assert.Contains(t, args, "--partial")
+	})
+
+	t.Run("--partial-dir", func(t *testing.T) {
+		args := getArguments(Options{
+			PartialDir: "test",
+		})
+		assert.Contains(t, args, "--partial-dir=test")
+	})
+
+	t.Run("--delay-updates", func(t *testing.T) {
+		args := getArguments(Options{
+			DelayUpdates: true,
+		})
+		assert.Contains(t, args, "--delay-updates")
+	})
+
+	t.Run("--prune-empty-dirs", func(t *testing.T) {
+		args := getArguments(Options{
+			PruneEmptyDirs: true,
+		})
+		assert.Contains(t, args, "--prune-empty-dirs")
+	})
+
+	t.Run("--numeric-ids", func(t *testing.T) {
+		args := getArguments(Options{
+			NumericIDs: true,
+		})
+		assert.Contains(t, args, "--numeric-ids")
+	})
+
+	t.Run("--timeout", func(t *testing.T) {
+		args := getArguments(Options{
+			Timeout: 100,
+		})
+		assert.Contains(t, args, "--timeout=100")
+	})
+
+	t.Run("--timeout", func(t *testing.T) {
+		args := getArguments(Options{
+			Contimeout: 100,
+		})
+		assert.Contains(t, args, "--contimeout=100")
+	})
+
+	t.Run("--ignore-times", func(t *testing.T) {
+		args := getArguments(Options{
+			IgnoreTimes: true,
+		})
+		assert.Contains(t, args, "--ignore-times")
+	})
+
+	t.Run("--size-only", func(t *testing.T) {
+		args := getArguments(Options{
+			SizeOnly: true,
+		})
+		assert.Contains(t, args, "--size-only")
+	})
+
+	t.Run("--modify-window", func(t *testing.T) {
+		args := getArguments(Options{
+			ModifyWindow: true,
+		})
+		assert.Contains(t, args, "--modify-window")
+	})
+
+	t.Run("--temp-dir", func(t *testing.T) {
+		args := getArguments(Options{
+			TempDir: "test",
+		})
+		assert.Contains(t, args, "--temp-dir=test")
+	})
+
+	t.Run("--fuzzy", func(t *testing.T) {
+		args := getArguments(Options{
+			Fuzzy: true,
+		})
+		assert.Contains(t, args, "--fuzzy")
+	})
+
+	t.Run("--compare-dest", func(t *testing.T) {
+		args := getArguments(Options{
+			CompareDest: "test",
+		})
+		assert.Contains(t, args, "--compare-dest=test")
+	})
+
+	t.Run("--copy-dest=", func(t *testing.T) {
+		args := getArguments(Options{
+			CopyDest: "test",
+		})
+		assert.Contains(t, args, "--copy-dest=test")
+	})
+
+	t.Run("--link-dest", func(t *testing.T) {
+		args := getArguments(Options{
+			LinkDest: "test",
+		})
+		assert.Contains(t, args, "--link-dest=test")
+	})
+
+	t.Run("--compress", func(t *testing.T) {
+		args := getArguments(Options{
+			Compress: true,
+		})
+		assert.Contains(t, args, "--compress")
+	})
+
+	t.Run("--compress-level", func(t *testing.T) {
+		args := getArguments(Options{
+			CompressLevel: 3,
+		})
+		assert.Contains(t, args, "--compress-level=3")
+	})
+
+	t.Run("--skip-compress=", func(t *testing.T) {
+		args := getArguments(Options{
+			SkipCompress: []string{"1", "2"},
+		})
+		assert.Contains(t, args, "--skip-compress=1,2")
+	})
+
+	t.Run("--cvs-exclude", func(t *testing.T) {
+		args := getArguments(Options{
+			CVSExclude: true,
+		})
+		assert.Contains(t, args, "--cvs-exclude")
+	})
+
+	t.Run("--stats", func(t *testing.T) {
+		args := getArguments(Options{
+			Stats: true,
+		})
+		assert.Contains(t, args, "--stats")
+	})
+
+	t.Run("--human-readable", func(t *testing.T) {
+		args := getArguments(Options{
+			HumanReadable: true,
+		})
+		assert.Contains(t, args, "--human-readable")
+	})
+
+	t.Run("--progress", func(t *testing.T) {
+		args := getArguments(Options{
+			Progress: true,
+		})
+		assert.Contains(t, args, "--progress")
+	})
+
+	t.Run("--ipv4", func(t *testing.T) {
+		args := getArguments(Options{
+			IPv4: true,
+		})
+		assert.Contains(t, args, "--ipv4")
+	})
+
+	t.Run("--ipv6", func(t *testing.T) {
+		args := getArguments(Options{
+			IPv6: true,
+		})
+		assert.Contains(t, args, "--ipv6")
+	})
 }
